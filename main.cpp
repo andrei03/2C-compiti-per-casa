1cc74d519721b61d44a0c845ca2c6f9508e96c16
andrei03 <galea.cojocaru@libero.it>
2017-04-11 20:37:18 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,197 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{   signed short int v;
+    v = 32767 ;
+    cout <<  " la variabile " << v << endl;
+    return 0;
+}
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{   long v;
+    v = 2147483647 ;
+    cout <<  " la variabile " << v << endl;
+    return 0;
+}
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{   unsigned short int v;
+    v = 32767 ;
+    cout <<  " la variabile " << v << endl;
+    return 0;
+}
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{   long int v;
+    v = 2147483647 ;
+    cout <<  " la variabile " << v << endl;
+    return 0;
+}
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{   int v;
+    v = 32767 ;
+    cout <<  " la variabile " << v << endl;
+    return 0;
+}
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{   signed long v;
+    v = 2147483647 ;
+    cout <<  " la variabile " << v << endl;
+    return 0;
+}
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{   short int v;
+    v = 32767 ;
+    cout <<  " la variabile " << v << endl;
+    return 0;
+}
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{   signed v;
+    v = 32767 ;
+    cout <<  " la variabile " << v << endl;
+    return 0;
+}
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{   signed long int v;
+    v = 2147483647 ;
+    cout <<  " la variabile " << v << endl;
+    return 0;
+}
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{   short v;
+    v = 32767 ;
+    cout <<  " la variabile " << v << endl;
+    return 0;
+}
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{   signed int v;
+    v = 32767 ;
+    cout <<  " la variabile " << v << endl;
+    return 0;
+}
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{   unsigned long int v;
+    v = 2147483647 ;
+    cout <<  " la variabile " << v << endl;
+    return 0;
+}
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{   signed short v;
+    v = 32767 ;
+    cout <<  " la variabile " << v << endl;
+    return 0;
+}
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{   unsigned int v;
+    v = 32767 ;
+    cout <<  " la variabile " << v << endl;
+    return 0;
+}
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{   unsigned short v;
+    v = 32767 ;
+    cout <<  " la variabile " << v << endl;
+    return 0;
+}
+
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{   unsigned int v;
+    v = 32767 ;
+    cout <<  " la variabile " << v << endl;
+    return 0;
+}
+
+
+
+
+
+
